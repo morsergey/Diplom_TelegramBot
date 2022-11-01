0d2fa42b5708cb2b6c69e7b0ba112fe6b673ab32
morsergey <workmsl@yandex.ru>
2022-11-01 08:50:13 +0300

    Update start.py

--- handlers/default_heandlers/start.py
+++ handlers/default_heandlers/start.py
@@ -6,7 +6,7 @@ from loader import bot
 @bot.message_handler(commands=['start'])
 def bot_start(message: Message):
 
-   welcome='''Здравствуйте! Я бот "Too Easy Travel" и помогу Вам выбрать отель мечты.
+   welcome=''' здравствуйте! Я бот "Too Easy Travel" и помогу Вам выбрать отель мечты.
     География поиска - весь мир. Я покажу вам сайты и фоторгафйии понравившихся отелей
     в соответствии с выбраными Вами характеристиками. Приступим.  
    '''
